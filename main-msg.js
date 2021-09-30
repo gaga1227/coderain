b203ab389cff2b7101130feb7f14982defe04141
Johnny Xu <gaga1227@gmail.com>
2021-09-30 12:01:23 +1000

    added query param default message

--- main-msg.js
+++ main-msg.js
@@ -2,32 +2,61 @@
  * msg module
  */
 (function msgModule() {
+  const win = window;
   const doc = document;
   const msgNode = doc.querySelector('#msgNode');
-
   if (msgNode === null) {
     return;
   }
 
+  /**
+   * Constants
+   */
+  const MSG_PARAM = 'msg';
+
+  /**
+   * View utils
+   */
+  const applyMsg = (msg = '') => {
+    if (typeof msg === 'string' && msg.trim().length >= 0) {
+      msgNode.textContent = msg;
+    }
+  };
   const clearMsg = () => {
-    msgNode.textContent = '';
+    applyMsg();
   };
   const clearFromHead = () => {
     const msgText = msgNode.textContent || '';
-    msgNode.textContent = msgText.substring(1);
+    applyMsg(msgText.substring(1));
   };
   const clearFromTail = () => {
     const msgText = msgNode.textContent || '';
-    msgNode.textContent = msgText.slice(0, -1);
+    applyMsg(msgText.slice(0, -1));
   };
-  const appendMsg = (e) => {
+
+  /**
+   * View methods
+   */
+  const applyInputMsg = (e) => {
     const newChar = e.key || '';
     // only allow intended keys
     if (newChar.length === 1) {
-      msgNode.textContent += newChar;
+      applyMsg(msgNode.textContent + newChar);
     }
   };
+  const applyDefaultMsg = () => {
+    let msgText;
 
+    // try get message from query param
+    const searchParams = new URLSearchParams(win.location.search);
+    msgText = searchParams.get(MSG_PARAM) || '';
+
+    applyMsg(msgText);
+  };
+
+  /**
+   * Event handlers
+   */
   const handleGlobalKeyUp = (e) => {
     switch (e.keyCode) {
       // escape
@@ -48,9 +77,20 @@
         break;
       // all others
       default:
-        appendMsg(e);
+        applyInputMsg(e);
     }
   };
-  doc.addEventListener('keyup', handleGlobalKeyUp);
-  doc.addEventListener('keydown', handleGlobalKeyDown);
+
+  /**
+   * Init
+   */
+  const init = () => {
+    // apply default given message text if any
+    applyDefaultMsg();
+
+    // register message inputs
+    doc.addEventListener('keyup', handleGlobalKeyUp);
+    doc.addEventListener('keydown', handleGlobalKeyDown);
+  };
+  init();
 })();
