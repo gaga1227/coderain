927ae5bd6b23e94ab53d76f0cb15f3692a80dfb4
Johnny Xu <gaga1227@users.noreply.github.com>
2021-10-05 20:58:34 +1100

    Create utils.v2.js

---
+++ assets/utils.v2.js
@@ -0,0 +1,44 @@
+/**
+ * throttle
+ * - https://stackoverflow.com/questions/27078285/simple-throttle-in-javascript
+ */
+const throttle = (callback, limit) => {
+  let waiting = false;
+  return function () {
+    if (!waiting) {
+      callback.apply(this, arguments);
+      waiting = true;
+      setTimeout(function () {
+        waiting = false;
+      }, limit);
+    }
+  }
+};
+
+/**
+ * debounce
+ * - https://gist.github.com/nmsdvid/8807205
+ */
+const debounce = (func, wait, immediate) => {
+  let timeout;
+  return function () {
+    var context = this, args = arguments;
+    clearTimeout(timeout);
+    timeout = setTimeout(function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    }, wait);
+    if (immediate && !timeout) func.apply(context, args);
+  };
+};
+
+/**
+ * Env helpers
+ */
+const isTouchDevice = () => {
+  return !!document && ('ontouchstart' in window || ('DocumentTouch' in window && document instanceof DocumentTouch));
+};
+const isFirefox = () => {
+  const userAgent = (navigator && navigator.userAgent || '').toLowerCase();
+  return userAgent.match(/(?:firefox|fxios)\/(\d+)/) !== null;
+};
