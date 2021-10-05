e3f1b6be1bb069c061eece484e5ac79ac754bcf4
Johnny Xu <gaga1227@users.noreply.github.com>
2021-10-05 22:31:17 +1100

    Update main.v2.js

--- assets/main.v2.js
+++ assets/main.v2.js
@@ -357,8 +357,8 @@ const drawFrameRate = throttle(() => {
  */
 const getTrasformOriginX = (rY) => {
   const pct = Math.round(rY * 100);
-  if (pct) < 0 pct = 0;
-  if (pct) > 100 pct = 100;
+  if (pct < 0) pct = 0;
+  if (pct > 100) pct = 100;
   return pct;
 }
 const getTrasformOriginY = (rX) => {
